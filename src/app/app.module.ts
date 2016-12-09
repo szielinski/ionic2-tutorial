06d3361292f4ed2ee0cd39ff5ae71507410a38ef
Szymon Zielinski <szymon.zielinski14@gmail.com>
2016-12-09 15:51:36 +0000

    Setup page skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { MyTeams } from './my-teams';
+import { Game } from './game';
+import { Teams } from './teams';
+import { Tournaments } from './tournaments';
+import { TeamDetail } from './team-detail';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    MyTeams,
+    Game,
+    Teams,
+    Tournaments,
+    TeamDetail
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +22,11 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    MyTeams,
+    Game,
+    Teams,
+    Tournaments,
+    TeamDetail
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
